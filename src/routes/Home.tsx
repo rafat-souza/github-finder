f2a5982cbe3216a64215416846ca73fce21be788
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-21 20:10:16 -0300

    Componente de busca

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Search from "../components/Search";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Search />
+    </div>
+  );
 };
 
 export default Home;
