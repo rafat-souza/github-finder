fdcd505992ae489fff082ea65db41f55e5452131
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-22 11:54:12 -0300

    Busca com enter do teclado

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,5 @@
 import { BsSearch } from "react-icons/bs";
-import { useState } from "react";
+import { useState, type KeyboardEvent } from "react";
 
 import classes from "./Search.module.css";
 
@@ -10,6 +10,12 @@ type SearchProps = {
 const Search = ({ loadUser }: SearchProps) => {
   const [userName, setUserName] = useState("");
 
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      loadUser(userName);
+    }
+  };
+
   return (
     <div className={classes.search}>
       <h2>Busque por um usuário:</h2>
@@ -19,6 +25,7 @@ const Search = ({ loadUser }: SearchProps) => {
           type="text"
           placeholder="Digite o nome do usuário"
           onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
         <button onClick={() => loadUser(userName)}>
           <BsSearch />
