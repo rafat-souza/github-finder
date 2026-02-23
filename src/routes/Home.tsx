290222cbf12ce0cb74c2004037025965554105d8
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-23 11:35:40 -0300

    Loader da busca

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,20 +3,25 @@ import { useState } from "react";
 import Search from "../components/Search";
 import User from "../components/User";
 import Error from "../components/Error";
+import Loader from "../components/Loader";
 
 import { type UserProps } from "../types/user";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
   const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const loadUser = async (userName: string) => {
+    setIsLoading(true);
     setError(false);
     setUser(null);
 
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    setIsLoading(false);
 
     if (res.status === 404) {
       setError(true);
@@ -39,6 +44,7 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
+      {isLoading && <Loader />}
       {user && <User {...user} />}
       {error && <Error />}
     </div>
